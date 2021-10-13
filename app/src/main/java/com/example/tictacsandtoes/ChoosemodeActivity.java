89b8c379f18cb65c078215c26843c8c0b4755b7e
NotJason123 <jason.r.vuilleumier@gmail.com>
2021-10-14 09:42:30 +1000

    Initial commit

---
+++ app/src/main/java/com/example/tictacsandtoes/ChoosemodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.tictacsandtoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoosemodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choosemode);
+    }
+}
