d2db007a013b191b5ccce07b1765316f54721862
NotJason123 <jason.r.vuilleumier@gmail.com>
2021-10-21 09:39:40 +1000

    singleplayer

--- app/src/main/java/com/example/tictacsandtoes/ChoosemodeActivity.java
+++ app/src/main/java/com/example/tictacsandtoes/ChoosemodeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,22 +12,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ChoosemodeActivity extends AppCompatActivity {
-
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choosemode);
-
+        mp = MediaPlayer.create(ChoosemodeActivity.this, R.raw.sound11);
 
     }
 
     public void Onclick(View view) {
         startActivity(new Intent(ChoosemodeActivity.this, Singleplayer.class));
-
+        mp.start();
     }
 }
-
 
 
 
--- app/src/main/java/com/example/tictacsandtoes/Singleplayer.java
+++ app/src/main/java/com/example/tictacsandtoes/Singleplayer.java
@@ -1,14 +1,26 @@
 package com.example.tictacsandtoes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Button;
+import android.view.View;
 import android.os.Bundle;
 
 public class Singleplayer extends AppCompatActivity {
+    MediaPlayer mp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleplayer);
+        mp = MediaPlayer.create(Singleplayer.this, R.raw.sound11);
+    }
+
+    public void Onclick2(View view){
+        startActivity(new Intent(Singleplayer.this, ChoosemodeActivity.class));
+        mp.start();
+
     }
 }
